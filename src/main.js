1daa048bfcd70c11ae48daa4932e5de5388c54de
semljola <johanlauri@yahoo.com>
2023-04-05 09:18:47 +0200

    Adds API Endpoint

--- src/main.js
+++ src/main.js
@@ -13,10 +13,75 @@ const simulation = d3.forceSimulation()
     .force('charge', d3.forceManyBody())
     .force('center', d3.forceCenter(width / 2, height / 2));
 
-async function getGPT4Response(prompt) {
-    // Implement the API call to GPT-4 here.
-    // The function should return an object with 'response', 'questions', and 'topic' properties.
+
+
+    async function getGPT4Response(prompt) {
+        const apiKey = 'your_api_key_here';
+        const apiEndpoint = 'https://api.openai.com/v1/engines/gpt-4/completions';
+
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`
+            },
+            body: JSON.stringify({
+                prompt: prompt,
+                max_tokens: 100,
+                n: 1,
+                stop: null,
+                temperature: 1.0,
+                top_p: 1,
+            })
+        };
+
+        try {
+            const response = await fetch(apiEndpoint, requestOptions);
+            const data = await response.json();
+            const generatedText = data.choices[0].text.trim();
+
+            // Custom parsing logic to extract response, questions, and topic from the generated text
+            const responseObject = parseGeneratedText(generatedText);
+
+            return responseObject;
+        } catch (error) {
+            console.error('Error calling GPT-4 API:', error);
+            return {
+                response: 'An error occurred while generating the text. Please check the console for details.',
+                questions: [],
+                topic: ''
+            };
+        }
+    
+    }
+function parseGeneratedText(text) {
+    // Replace these parsing functions with your own logic to extract the required information
+    const response = extractResponse(text);
+    const questions = extractQuestions(text);
+    const topic = extractTopic(text);
+
+    return {
+        response: response,
+        questions: questions,
+        topic: topic
+    };
 }
+
+function extractResponse(text) {
+    // Your logic to extract the response from the generated text
+    return text;
+}
+
+function extractQuestions(text) {
+    // Your logic to extract the questions from the generated text
+    return [];
+}
+
+function extractTopic(text) {
+    // Your logic to extract the topic from the generated text
+    return '';
+}
+
 
 function updateGraph(newNodes, newLinks) {
     // Update the nodes and links arrays.
